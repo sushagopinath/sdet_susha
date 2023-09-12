e4dedacb29a3044988b632b718c6c3cb00a53ee4
Susha <sushagopinath@gmail.com>
2023-09-12 20:45:56 +0530

    adding another get method

---
+++ Api_RestAssured/src/test/java/APIAugust/Api_RestAssured/GetMethod.java
@@ -0,0 +1,15 @@
+package APIAugust.Api_RestAssured;
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class GetMethod {
+
+	public static void main(String[] args) {
+		Response rs=given().contentType(ContentType.JSON).when().get("http://localhost:3000/RestDummyAPI1");
+		System.out.println(rs.statusCode());
+		System.out.println(rs.asString());
+	}
+
+}
