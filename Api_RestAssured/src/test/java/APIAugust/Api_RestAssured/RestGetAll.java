fe70960c3a86326265eb968672f7d48ce9e9f662
Susha <sushagopinath@gmail.com>
2023-09-12 16:42:49 +0530

    first commit

---
+++ Api_RestAssured/src/test/java/APIAugust/Api_RestAssured/RestGetAll.java
@@ -0,0 +1,23 @@
+package APIAugust.Api_RestAssured;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class RestGetAll {
+public static void main(String[] args) {
+		
+		Response rs=
+		given()
+		.contentType(ContentType.JSON)
+		.when()
+		.get(" http://localhost:3000/RestDummyAPI1/5");
+		System.out.println("status code is:");
+		System.out.println(rs.statusCode());
+		System.out.println("response data is:");
+		System.out.println(rs.asString());
+		
+	}
+
+}
